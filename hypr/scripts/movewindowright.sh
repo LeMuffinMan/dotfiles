c759ed419aa5d134d912377f4451315454e4e7b3
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2025-01-12 03:34:07 +0100

    bindings

---
+++ hypr/scripts/movewindowright.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+WINX=$(hyprctl activewindow | grep "at: " | awk '{print$2}' | awk -F ',' '{print$1}')
+WINY=$(hyprctl activewindow | grep "at: " | awk '{print$2}' | awk -F ',' '{print$2}')
+
+hyprctl dispatch movewindow r
+
+
