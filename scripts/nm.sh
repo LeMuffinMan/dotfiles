7ccc3a3e156e008c40300b2bd621c4611f394d6e
muffin <muffin@arch.man>
2025-01-13 23:47:23 +0100

    Not one conflict, today was a good day.

--- scripts/nm.sh
+++ scripts/nm.sh
@@ -3,3 +3,5 @@
 kitty --hold --class "netman" -e bash -c "echo -e 'Scanning wifi ...\n';
                         nmcli device wifi list;
                         echo -e '\nnmcli device wifi connect SSID/BSSID --ask';";
+
+
