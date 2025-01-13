1a01c4a772da066a05aa857e1e3a7da81e4932b7
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2025-01-13 01:49:13 +0100

    c&p fail

--- scripts/git.sh
+++ scripts/git.sh
@@ -3,8 +3,16 @@
 cd /home/muffin/dotfiles/
 git status
 echo 'Yolo commit ? y/n'
-git add *
-git yolo
-echo "commit : $(git log | head -n 5 | tail -n 1)"
-read
+read YN
+if [[ $YN == 'y' ]];
+then
+  git add *
+  git yolo
+  echo "commit : $(git log | head -n 5 | tail -n 1)"
+  git push
+  echo 'Press any key to quit'
+  read
+else
+  zsh
+fi
 
