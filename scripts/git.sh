b9753c487c16ccb623b0887112dffd58992b01e8
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2025-01-13 01:50:24 +0100

    magic, have no clue but it works

--- scripts/git.sh
+++ scripts/git.sh
@@ -2,13 +2,16 @@
 
 cd /home/muffin/dotfiles/
 git status
+echo ''
 echo 'Yolo commit ? y/n'
 read YN
 if [[ $YN == 'y' ]];
 then
   git add *
   git yolo
+  echo ''
   echo "commit : $(git log | head -n 5 | tail -n 1)"
+  echo ''
   git push
   echo 'Press any key to quit'
   read
